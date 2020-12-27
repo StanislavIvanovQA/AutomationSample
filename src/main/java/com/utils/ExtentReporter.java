c875786273a70710d88b0a7422304f28311fe61f
@stanislav.ivanov <stanislav.ivanov@vrpconsulting.com>
2020-12-27 20:04:19 +0300

    deleted unnecessary imports

--- src/main/java/com/utils/ExtentReporter.java
+++ src/main/java/com/utils/ExtentReporter.java
@@ -1,12 +1,7 @@
 package com.utils;
 
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 public class ExtentReporter {
     private static ExtentReports extent;
